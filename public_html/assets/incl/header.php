ab5e61154030ed8f052c6fb25437f103a4652c81
Heinz K <heka@techcollege.dk>
2020-12-07 13:35:38 +0100

    Header Update

--- public_html/assets/incl/header.php
+++ public_html/assets/incl/header.php
@@ -3,7 +3,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="#" type="image/x-icon">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <script src="/assets/js/script.js" defer></script>
     <title><?php echo $strPageTitle ?></title>
 </head>
 <body>
