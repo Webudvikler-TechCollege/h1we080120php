ef6a3e8f38b06912857ce5bc156f37d6809359c3
Heinz K <heka@techcollege.dk>
2020-12-10 14:50:37 +0100

    PDO Updates

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,30 @@
 <?php
 $strPageTitle = 'Forside';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/incl/header.php';
-?>
-<!-- Her slutter min header -->
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat porro suscipit consectetur error id iure aperiam nam quidem, fuga libero! Perspiciatis cum enim assumenda molestias laboriosam iusto error, qui esse!
-<!-- Her starter min footer -->
-<?php
+
+$dns = "mysql: host=localhost;dbname=songbook;charset=utf8";
+$username = "heka";
+$password = "password";
+
+try {
+    
+    $db = new PDO($dns, $username, $password);
+
+    $id = (int)$_GET["id"];
+    var_dump($id);
+
+    $sql = "SELECT * FROM song WHERE id = :id";
+    $stm = $db->prepare($sql);
+    $stm->bindParam(":id", $id);
+    $stm->execute();
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+    var_dump($result);
+
+    $db = null;
+
+} catch(PDOException $error) {
+    echo 'Fejl i tilslutning til database: ' . $error;
+}
+
 require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/incl/footer.php';
