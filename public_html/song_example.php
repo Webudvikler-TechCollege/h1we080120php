fe3c92c93d3b8dba10a6789bbd828c03a72aad97
Heinz K <heka@techcollege.dk>
2020-12-14 11:29:55 +0100

    Init og klasse update

--- public_html/song_example.php
+++ public_html/song_example.php
@@ -1,5 +1,8 @@
 <?php
+// Relativ reference til klasse fil
 //require_once '../core/classes/song.php';
+
+// Absolut reference til init fil som inkluderer vores autoloader
 require_once $_SERVER['DOCUMENT_ROOT'] . '/assets/incl/init.php';
 
 $strPageTitle = 'Eksempel på brug af en klassefil';
